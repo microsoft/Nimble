11f1de3d0f057452f70ef4305608ff039643e42e
Srinath Setty <srinath@microsoft.com>
2021-06-25 10:46:56 -0700

    run cargo fmt

--- Nimble/src/coordinator.rs
+++ Nimble/src/coordinator.rs
@@ -1,129 +1,140 @@
 mod store;
 
+use crate::store::Store;
+use protocol::call_server::{Call, CallServer};
+use protocol::{Data, LedgerRequest, LedgerResponse, Query, UpdateQuery};
+use std::collections::HashMap;
+use std::sync::{Arc, RwLock};
 use tonic::transport::Server;
 use tonic::{Request, Response, Status};
-use protocol::call_server::{Call, CallServer};
-use protocol::{Data, LedgerRequest, LedgerResponse, UpdateQuery, Query};
-use std::collections::{HashMap};
 use uuid::Uuid;
-use std::sync::{Arc, RwLock};
-use crate::store::Store;
 
 pub mod protocol {
-    tonic::include_proto!("protocol");
+  tonic::include_proto!("protocol");
 }
 
 #[derive(Debug, Default)]
 pub struct CoordinatorState {
-    state: Arc<RwLock<Store>>
+  state: Arc<RwLock<Store>>,
 }
 
 #[derive(Debug, Default)]
-pub struct CallServiceStub{}
+pub struct CallServiceStub {}
 
 impl CoordinatorState {
-    pub fn new() -> Self {
-        CoordinatorState {
-            state: Arc::new(RwLock::new(Store {
-                ledgers: HashMap::new()
-            })),
-        }
+  pub fn new() -> Self {
+    CoordinatorState {
+      state: Arc::new(RwLock::new(Store {
+        ledgers: HashMap::new(),
+      })),
     }
+  }
 }
 
 #[tonic::async_trait]
 impl Call for CoordinatorState {
-    async fn new_ledger(&self, request: Request<LedgerRequest>,)
-                        -> Result<Response<LedgerResponse>, Status> {
-        let LedgerRequest { name } = request.into_inner();
+  async fn new_ledger(
+    &self,
+    request: Request<LedgerRequest>,
+  ) -> Result<Response<LedgerResponse>, Status> {
+    let LedgerRequest { name } = request.into_inner();
 
-        println!("Received a NewLedger Request : {:?}", name);
-        let unique_ledger_id = Uuid::new_v4();
-        println!("{:?}", unique_ledger_id.to_string());
-        let reply = protocol::LedgerResponse {
-            handle: unique_ledger_id.to_string(),
-        };
-        let value: Vec<u8> = name.as_bytes().to_vec();
+    println!("Received a NewLedger Request : {:?}", name);
+    let unique_ledger_id = Uuid::new_v4();
+    println!("{:?}", unique_ledger_id.to_string());
+    let reply = protocol::LedgerResponse {
+      handle: unique_ledger_id.to_string(),
+    };
+    let value: Vec<u8> = name.as_bytes().to_vec();
 
-        let mut store = self.state.write().expect("Failed to acquire lock on state");
-        store.set(unique_ledger_id.to_string(), value);
+    let mut store = self.state.write().expect("Failed to acquire lock on state");
+    store.set(unique_ledger_id.to_string(), value);
 
-        let r = store.get(unique_ledger_id.to_string());
-        println!("Something in state: {:?}", r);
+    let r = store.get(unique_ledger_id.to_string());
+    println!("Something in state: {:?}", r);
 
-        let ledgers_available = store.get_all_ledgers_handles();
-        println!("Ledgers in State: {:?}", ledgers_available);
+    let ledgers_available = store.get_all_ledgers_handles();
+    println!("Ledgers in State: {:?}", ledgers_available);
 
-        Ok(Response::new(reply))
-    }
+    Ok(Response::new(reply))
+  }
 
-    async fn append_to_ledger(&self, request: Request<UpdateQuery>,)
-        -> Result<Response<protocol::Status>, Status> {
-        let UpdateQuery {handle, value} = request.into_inner();
-        println!("Received a AppendToLedger Request : {:?} {:?}", handle, value);
+  async fn append_to_ledger(
+    &self,
+    request: Request<UpdateQuery>,
+  ) -> Result<Response<protocol::Status>, Status> {
+    let UpdateQuery { handle, value } = request.into_inner();
+    println!(
+      "Received a AppendToLedger Request : {:?} {:?}",
+      handle, value
+    );
 
-        let mut store = self.state.write().expect("Failed to acquire lock on state");
-        let content: Vec<u8> = value.unwrap().content;
-        store.set(handle, content.to_vec());
+    let mut store = self.state.write().expect("Failed to acquire lock on state");
+    let content: Vec<u8> = value.unwrap().content;
+    store.set(handle, content.to_vec());
 
-        let reply = protocol::Status {
-            status: true,
-        };
+    let reply = protocol::Status { status: true };
 
-        Ok(Response::new(reply))
-    }
+    Ok(Response::new(reply))
+  }
 
-    async fn read_latest(&self, request: Request<Query>,)
-                              -> Result<Response<protocol::Response>, Status> {
-        let Query { handle, index: _ } = request.into_inner();
-        // index has to ideally not exist in the query, TODO: explore "optional"
-        println!("Received a ReadLatest Request : {:?}", handle);
+  async fn read_latest(
+    &self,
+    request: Request<Query>,
+  ) -> Result<Response<protocol::Response>, Status> {
+    let Query { handle, index: _ } = request.into_inner();
+    // index has to ideally not exist in the query, TODO: explore "optional"
+    println!("Received a ReadLatest Request : {:?}", handle);
 
-        let value = self.state.read().expect("Failed to acquire read lock")
-            .get_latest_state_of_ledger(handle);
+    let value = self
+      .state
+      .read()
+      .expect("Failed to acquire read lock")
+      .get_latest_state_of_ledger(handle);
 
-        let reply = protocol::Response {
-            value: Some(Data {
-                content: value
-            }),
-        };
+    let reply = protocol::Response {
+      value: Some(Data { content: value }),
+    };
 
-        Ok(Response::new(reply))
-    }
+    Ok(Response::new(reply))
+  }
 
-    async fn read_at_index(&self, request: Request<Query>,)
-                              -> Result<Response<protocol::Response>, Status> {
-        println!("Received a ReadAtIndex Request : {:?}", request);
+  async fn read_at_index(
+    &self,
+    request: Request<Query>,
+  ) -> Result<Response<protocol::Response>, Status> {
+    println!("Received a ReadAtIndex Request : {:?}", request);
 
-        let Query { handle, index } = request.into_inner();
-        // index has to ideally not exist in the query, TODO: explore "optional"
-        println!("Received a ReadLatest Request : {:?}", handle);
+    let Query { handle, index } = request.into_inner();
+    // index has to ideally not exist in the query, TODO: explore "optional"
+    println!("Received a ReadLatest Request : {:?}", handle);
 
-        let value = self.state.read().expect("Failed to acquire read lock")
-            .get_ledger_state_at_index(handle, index);
+    let value = self
+      .state
+      .read()
+      .expect("Failed to acquire read lock")
+      .get_ledger_state_at_index(handle, index);
 
-        let reply = protocol::Response {
-            value: Some(Data {
-                content: value
-            }),
-        };
+    let reply = protocol::Response {
+      value: Some(Data { content: value }),
+    };
 
-        Ok(Response::new(reply))
-    }
+    Ok(Response::new(reply))
+  }
 }
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
-    #[rustfmt::skip]
+  #[rustfmt::skip]
     let addr = "[::1]:8080".parse()?;
-    let server = CoordinatorState::new();
+  let server = CoordinatorState::new();
 
-    println!("Running gRPC Coordinator Service at {:?}", addr);
+  println!("Running gRPC Coordinator Service at {:?}", addr);
 
-    Server::builder()
-        .add_service(CallServer::new(server))
-        .serve(addr)
-        .await?;
+  Server::builder()
+    .add_service(CallServer::new(server))
+    .serve(addr)
+    .await?;
 
-    Ok(())
+  Ok(())
 }
--- Nimble/src/store.rs
+++ Nimble/src/store.rs
@@ -3,44 +3,44 @@ use std::convert::TryFrom;
 
 #[derive(Debug, Default)]
 pub struct Store {
-    pub ledgers: HashMap<String, Vec<Vec<u8>>>
+  pub ledgers: HashMap<String, Vec<Vec<u8>>>,
 }
 
 impl Store {
-    pub fn set(&mut self, key: String, value: Vec<u8>) {
-        println!("Setting State : {:?} {:?}", key, value);
-        if self.ledgers.contains_key(&*key) {
-            let ledgers = self.ledgers.clone();
-            let (state_k, state_v) = ledgers.get_key_value(&*key).unwrap();
-            let mut updated_state = Vec::new();
-            updated_state.append(&mut state_v.to_vec());
-            updated_state.push(value);
-            println!("Updated State: {:?} --> {:?}", state_v, updated_state);
-            self.ledgers.insert(state_k.to_string(), updated_state);
-        } else {
-            self.ledgers.entry(key).or_default().push(value);
-        }
+  pub fn set(&mut self, key: String, value: Vec<u8>) {
+    println!("Setting State : {:?} {:?}", key, value);
+    if self.ledgers.contains_key(&*key) {
+      let ledgers = self.ledgers.clone();
+      let (state_k, state_v) = ledgers.get_key_value(&*key).unwrap();
+      let mut updated_state = Vec::new();
+      updated_state.append(&mut state_v.to_vec());
+      updated_state.push(value);
+      println!("Updated State: {:?} --> {:?}", state_v, updated_state);
+      self.ledgers.insert(state_k.to_string(), updated_state);
+    } else {
+      self.ledgers.entry(key).or_default().push(value);
     }
+  }
 
-    pub fn get(&self, key: String) -> Vec<Vec<u8>> {
-        self.ledgers.get(&*key).unwrap().to_vec()
+  pub fn get(&self, key: String) -> Vec<Vec<u8>> {
+    self.ledgers.get(&*key).unwrap().to_vec()
+  }
+
+  pub fn get_latest_state_of_ledger(&self, key: String) -> Vec<u8> {
+    self.get(key).last().unwrap().to_vec()
+  }
+
+  pub fn get_ledger_state_at_index(&self, key: String, mut index: u64) -> Vec<u8> {
+    let ledger = self.get(key);
+
+    if !(index < ledger.len() as u64) {
+      index = 0
     }
+    let usize_index = usize::try_from(index).unwrap();
+    ledger[usize_index].to_vec()
+  }
 
-    pub fn get_latest_state_of_ledger(&self, key: String) -> Vec<u8> {
-        self.get(key).last().unwrap().to_vec()
-    }
-
-    pub fn get_ledger_state_at_index(&self, key: String, mut index: u64) -> Vec<u8> {
-        let ledger = self.get(key);
-
-        if !(index < ledger.len() as u64) {
-            index = 0
-        }
-        let usize_index = usize::try_from(index).unwrap();
-        ledger[usize_index].to_vec()
-    }
-
-    pub fn get_all_ledgers_handles(&self) -> Vec<String> {
-        self.ledgers.keys().cloned().collect()
-    }
+  pub fn get_all_ledgers_handles(&self) -> Vec<String> {
+    self.ledgers.keys().cloned().collect()
+  }
 }
