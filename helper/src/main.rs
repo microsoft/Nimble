da468a7fb7f17645384084822b3211587872db19
Sebastian Angel <sangel@microsoft.com>
2022-04-22 14:40:18 -0400

    perf measurement for inserts (#158)
    * perf measurement for inserts
    
    * create new ledger and then do one insert

--- helper/src/main.rs
+++ helper/src/main.rs
@@ -1,5 +1,7 @@
 use clap::{App, Arg};
 use mongodb::Client;
+use serde::{Deserialize, Serialize};
+use std::time::Instant;
 
 async fn reset_cosmosdb(conn_string: &str, dbname: &str) {
   let res = Client::with_uri_str(conn_string).await;
@@ -15,6 +17,53 @@ async fn reset_cosmosdb(conn_string: &str, dbname: &str) {
     .await
     .expect("failed to delete ledgers");
 
+  println!("reset database {}", dbname);
+}
+
+#[derive(Clone, Serialize, Deserialize, Debug)]
+struct DBEntry {
+  index: i64,
+  value: String,
+}
+
+async fn perf_test(conn_string: &str, dbname: &str) {
+  let res = Client::with_uri_str(conn_string).await;
+  if res.is_err() {
+    eprintln!("Connection with cosmosdb failed");
+    return;
+  }
+  let cosmos_client = res.unwrap();
+
+  let mut value = String::new();
+  for _ in 1..900 {
+    value.push('a');
+  }
+  let total: i64 = 100;
+  let now = Instant::now();
+  for i in 1..total {
+    let ledger = cosmos_client
+      .database(dbname)
+      .collection::<DBEntry>(&format!("perftest{}", i));
+    ledger
+      .insert_one(
+        DBEntry {
+          index: i,
+          value: value.clone(),
+        },
+        None,
+      )
+      .await
+      .expect("this should work");
+  }
+  println!(
+    "Average time over 100 operations is {} ms",
+    now.elapsed().as_millis() / (total as u128)
+  );
+  cosmos_client
+    .database(dbname)
+    .drop(None)
+    .await
+    .expect("failed to delete ledgers");
   println!("reset database {}", dbname);
 }
 
@@ -50,6 +99,9 @@ async fn main() {
   match action {
     "reset" => {
       reset_cosmosdb(cosmos, dbname).await;
+    },
+    "perf" => {
+      perf_test(cosmos, dbname).await;
     },
     _ => {
       panic!("Unknown action {}", action);
