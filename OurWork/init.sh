bf174c3cead6ff7603d8fdffc6dc68baae5104ad
Sherif Hussien <sherifhussien1996@gmail.com>
2024-10-27 13:13:51 +0100

    udpate init script

--- OurWork/init.sh
+++ OurWork/init.sh
@@ -1,4 +1,5 @@
 #! /bin/bash
+SSH_AUTH_SOCK= ssh -v -F /dev/null -i <path/to/privkey> -oProxyCommand="ssh tunnel@login.dos.cit.tum.de -i <path/to/privkey> -W %h:%p" <yourusername>@vislor.dos.cit.tum.de
 
 curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
 
