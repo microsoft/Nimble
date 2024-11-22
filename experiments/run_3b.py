33821e5f252071d584f022ee0ec0d47df28fe7a4
BuildTools <unconfigured@null.spigotmc.org>
2024-11-22 16:56:29 +0100

    Modifying code to accept Azurite tables instead of azure. Setum ain comments at the top

--- experiments/run_3b.py
+++ experiments/run_3b.py
@@ -1,9 +1,33 @@
 import os
+import subprocess
 import time
 import random
+
+import logging
+
 from config import *
 from setup_nodes import *
 from datetime import datetime
+
+#
+#Usage:
+# 1. Go to OurWork/AAzurite
+# 2. npm install -g azurite
+# 3. start Azurite in the background: azurite --silent --location ./azurite_data --debug ./azurite_debug.log --tableHost 127.0.0.1 --tablePort 10002 &
+# 4. Verify it is running: ps aux | grep azurite
+#
+
+# Azurite default configuration
+AZURITE_ACCOUNT_NAME = "devstoreaccount1"
+AZURITE_ACCOUNT_KEY = "Eby8vdM02xWkA3az9W5ZPcuwwd2E9aMJW6DhDeUpgw=fGzv3nwKONNlGRd29aZJof7PRwIgORJFjBRzq=C41vHcP9mlX1Ag=="
+AZURITE_ENDPOINT = "http://127.0.0.1:10002/devstoreaccount1"
+
+# Environment check for Azurite
+if not os.environ.get('STORAGE_MASTER_KEY', ''):
+    os.environ['STORAGE_MASTER_KEY'] = AZURITE_ACCOUNT_KEY
+
+if not os.environ.get('STORAGE_ACCOUNT_NAME', ''):
+    os.environ['STORAGE_ACCOUNT_NAME'] = AZURITE_ACCOUNT_NAME
 
 timestamp = time.time()
 dt_object = datetime.fromtimestamp(timestamp)
@@ -13,12 +37,31 @@ EXP_NAME = "fig-3b-" + dt_string
 NUM_ITERATIONS = 1
 
 # Our table implementation can support much higher throughput for reads than create or append
-CREATE_APPEND_LOAD = [2000] #[500, 1000, 1500, 2000, 2500]  # requests/second
-READ_LOAD = [50000] # CREATE_APPEND_LOAD + [10000, 15000, 25000, 50000, 55000]
+CREATE_APPEND_LOAD = [2000]  # [500, 1000, 1500, 2000, 2500] requests/second
+READ_LOAD = [50000]  # CREATE_APPEND_LOAD + [10000, 15000, 25000, 50000, 55000]
+
+
+# Setup logging
+def setup_logging(log_folder):
+    if not os.path.exists(log_folder):
+        os.makedirs(log_folder)
+
+    log_file = os.path.join(log_folder, "experiment.log")
+
+    logging.basicConfig(
+        filename=log_file,
+        level=logging.DEBUG,
+        format='%(asctime)s - %(levelname)s - %(message)s',
+    )
+
 
 def run_3b(time, op, out_folder):
     load = CREATE_APPEND_LOAD
 
+    setup_logging(out_folder)
+    log_dir = os.path.dirname("./logs")
+    if not os.path.exists(log_dir):
+        os.makedirs(log_dir)
     if op == "read":
         load = READ_LOAD
 
@@ -30,11 +73,24 @@ def run_3b(time, op, out_folder):
         cmd += " -- " + str(i) + "req"
         cmd += " > " + out_folder + op + "-" + str(i) + ".log\'"
 
+        logging.info(f"Executing command: {cmd}")
+
         cmd = ssh_cmd(SSH_IP_CLIENT, cmd)
 
         print(cmd)
-        os.system(cmd)
+        result = subprocess.run(cmd, shell=True, capture_output=True)
 
+        if result.returncode != 0:
+            logging.error(f"Command failed with return code: {result.returncode}")
+            logging.error(f"Standard Output: {result.stdout.decode()}")
+            logging.error(f"Standard Error: {result.stderr.decode()}")
+            print(f"An error happened with : {cmd} \n Error output: {result.stderr.decode()}\n\n")
+        else:
+            logging.info(f"Command executed successfully. Output captured in: {out_folder}{op}-{i}.log")
+            print(f"Command executed successfully. Output captured in: {out_folder}{op}-{i}.log")
+
+
+# Ensure environment variables are set for Azurite
 if os.environ.get('STORAGE_MASTER_KEY', '') == "" or os.environ.get('STORAGE_ACCOUNT_NAME', '') == "":
     print("Make sure to set the STORAGE_MASTER_KEY and STORAGE_ACCOUNT_NAME environment variables")
     exit(-1)
@@ -42,8 +98,10 @@ if os.environ.get('STORAGE_MASTER_KEY', '') == "" or os.environ.get('STORAGE_ACC
 out_folder = OUTPUT_FOLDER + "/" + EXP_NAME + "/"
 setup_output_folder(SSH_IP_CLIENT, out_folder)
 
-store = " -s table -n nimble" + str(random.randint(1,100000000)) + " -a \"" + os.environ['STORAGE_ACCOUNT_NAME'] + "\""
-store += " -k \"" + os.environ['STORAGE_MASTER_KEY'] + "\""
+# Replace Azure Table Storage connection string with Azurite's
+store = f" -s table -n nimble{random.randint(1, 100000000)} -a \"{os.environ['STORAGE_ACCOUNT_NAME']}\""
+store += f" -k \"{os.environ['STORAGE_MASTER_KEY']}\""
+store += f" --endpoint \"{AZURITE_ENDPOINT}\""
 
 for i in range(NUM_ITERATIONS):
     teardown(False)
@@ -62,7 +120,7 @@ for i in range(NUM_ITERATIONS):
     # Read from the ledgers
     operation = "read"
     duration = "30s"
-    run_3b(duration, operation, out_folder)
+    run_3b(duration, out_folder)
 
 teardown(False)
 collect_results(SSH_IP_CLIENT)
--- experiments/setup_nodes.py
+++ experiments/setup_nodes.py
@@ -2,156 +2,117 @@ import os
 import time
 from config import *
 
-# make sure to set the configuration in config.py
+#
+#Usage:
+# 1. Go to OurWork/AAzurite
+# 2. npm install -g azurite
+# 3. start Azurite in the background: azurite --silent --location ./azurite_data --debug ./azurite_debug.log --tableHost 127.0.0.1 --tablePort 10002 &
+# 4. Verify it is running: ps aux | grep azurite
+#
 
-CMD = "screen -d -m " + NIMBLE_BIN_PATH
-HAS_LB = LISTEN_IP_ENDPOINT_1 != LISTEN_IP_LOAD_BALANCER # if not the same, we assume 2 endpoints and a load balancer
+# Default Azurite Configuration
+AZURITE_ACCOUNT_NAME = "devstoreaccount1"
+AZURITE_ACCOUNT_KEY = "Eby8vdM02xWkA3az9W5ZPcuwwd2E9aMJW6DhDeUpgw=fGzv3nwKONNlGRd29aZJof7PRwIgORJFjBRzq=C41vHcP9mlX1Ag=="
+AZURITE_ENDPOINT = "http://127.0.0.1:10002/devstoreaccount1"
 
+# Update Azurite connection settings
+os.environ['STORAGE_ACCOUNT_NAME'] = AZURITE_ACCOUNT_NAME
+os.environ['STORAGE_MASTER_KEY'] = AZURITE_ACCOUNT_KEY
+
+# Modify this command for local Azurite usage
+CMD = f"screen -d -m {NIMBLE_BIN_PATH}"
+
+# Determine if there are distinct endpoints for load balancer
+HAS_LB = LISTEN_IP_ENDPOINT_1 != LISTEN_IP_LOAD_BALANCER
+
+# Helper function for executing commands locally or remotely
+def ssh_cmd(ip, cmd):
+    if LOCAL_RUN:
+        return cmd.replace('\'', '')
+    else:
+        return f"ssh -o StrictHostKeyChecking=no -i {SSH_KEY_PATH} {SSH_USER}@{ip} {cmd}"
+
+# Helper function to create output folder on remote or local
+def setup_output_folder(ip, out_folder):
+    folder_cmd = ssh_cmd(ip, f"\'mkdir -p {out_folder}\'")
+    print(folder_cmd)
+    os.system(folder_cmd)
+
+# Helper function to collect results from a remote machine
+def collect_results(ip):
+    if LOCAL_RUN:
+        return ""
+    else:
+        cmd = f"scp -r -i {SSH_KEY_PATH} {SSH_USER}@{ip}:{OUTPUT_FOLDER} ./"
+        print(cmd)
+        os.system(cmd)
+
+# Setting up endorsers (main, backup, SGX)
 def setup_main_endorsers():
-    endorser1 = ssh_cmd(SSH_IP_ENDORSER_1, CMD + "/endorser -t " + LISTEN_IP_ENDORSER_1 + " -p " + PORT_ENDORSER_1)
-    endorser2 = ssh_cmd(SSH_IP_ENDORSER_2, CMD + "/endorser -t " + LISTEN_IP_ENDORSER_2 + " -p " + PORT_ENDORSER_2)
-    endorser3 = ssh_cmd(SSH_IP_ENDORSER_3, CMD + "/endorser -t " + LISTEN_IP_ENDORSER_3 + " -p " + PORT_ENDORSER_3)
-
-    print(endorser1)
-    os.system(endorser1)
-    print(endorser2)
-    os.system(endorser2)
-    print(endorser3)
-    os.system(endorser3)
-
-    time.sleep(5)
-
-def setup_backup_endorsers():
-    endorser4 = ssh_cmd(SSH_IP_ENDORSER_4, CMD + "/endorser -t " + LISTEN_IP_ENDORSER_4 + " -p " + PORT_ENDORSER_4)
-    endorser5 = ssh_cmd(SSH_IP_ENDORSER_5, CMD + "/endorser -t " + LISTEN_IP_ENDORSER_5 + " -p " + PORT_ENDORSER_5)
-    endorser6 = ssh_cmd(SSH_IP_ENDORSER_6, CMD + "/endorser -t " + LISTEN_IP_ENDORSER_6 + " -p " + PORT_ENDORSER_6)
-
-    print(endorser4)
-    os.system(endorser4)
-    print(endorser5)
-    os.system(endorser5)
-    print(endorser6)
-    os.system(endorser6)
+    endorsers = [
+        (SSH_IP_ENDORSER_1, LISTEN_IP_ENDORSER_1, PORT_ENDORSER_1),
+        (SSH_IP_ENDORSER_2, LISTEN_IP_ENDORSER_2, PORT_ENDORSER_2),
+        (SSH_IP_ENDORSER_3, LISTEN_IP_ENDORSER_3, PORT_ENDORSER_3),
+    ]
+    for ip, listen_ip, port in endorsers:
+        cmd = ssh_cmd(ip, f"{CMD}/endorser -t {listen_ip} -p {port}")
+        print(cmd)
+        os.system(cmd)
 
     time.sleep(5)
 
-def setup_sgx_endorsers():
-    endorser1 = "screen -d -m " + NIMBLE_PATH + "/endorser-openenclave/host/endorser_host "
-    endorser1 += NIMBLE_PATH + "/endorser-openenclave/enclave/enclave-sgx2.signed "
-    endorser1 += "-p " + PORT_SGX_ENDORSER_1
-    endorser1 = ssh_cmd(SSH_IP_SGX_ENDORSER_1, endorser1)
-
-    endorser2 = "screen -d -m " + NIMBLE_PATH + "/endorser-openenclave/host/endorser_host "
-    endorser2 += NIMBLE_PATH + "/endorser-openenclave/enclave/enclave-sgx2.signed "
-    endorser2 += "-p " + PORT_SGX_ENDORSER_2
-    endorser2 = ssh_cmd(SSH_IP_SGX_ENDORSER_2, endorser2)
-
-    endorser3 = "screen -d -m " + NIMBLE_PATH + "/endorser-openenclave/host/endorser_host "
-    endorser3 += NIMBLE_PATH + "/endorser-openenclave/enclave/enclave-sgx2.signed "
-    endorser3 += "-p " + PORT_SGX_ENDORSER_3
-    endorser3 = ssh_cmd(SSH_IP_SGX_ENDORSER_3, endorser3)
-
-    print(endorser1)
-    os.system(endorser1)
-    print(endorser2)
-    os.system(endorser2)
-    print(endorser3)
-    os.system(endorser3)
-
-    time.sleep(30) # they take much longer to boot
-
-
+# Setting up the coordinator
 def setup_coordinator(store):
-    coordinator = CMD + "/coordinator -t " + LISTEN_IP_COORDINATOR + " -p " + PORT_COORDINATOR + " -r " + PORT_COORDINATOR_CTRL
-    coordinator += " -e \"http://" + LISTEN_IP_ENDORSER_1 + ":" + PORT_ENDORSER_1
-    coordinator += ",http://" + LISTEN_IP_ENDORSER_2 + ":" + PORT_ENDORSER_2
-    coordinator += ",http://" + LISTEN_IP_ENDORSER_3 + ":" + PORT_ENDORSER_3
-    coordinator += "\" -l 60"
-    coordinator += store
+    coordinator = f"{CMD}/coordinator -t {LISTEN_IP_COORDINATOR} -p {PORT_COORDINATOR} -r {PORT_COORDINATOR_CTRL} "
+    coordinator += f"-e \"http://{LISTEN_IP_ENDORSER_1}:{PORT_ENDORSER_1},http://{LISTEN_IP_ENDORSER_2}:{PORT_ENDORSER_2},"
+    coordinator += f"http://{LISTEN_IP_ENDORSER_3}:{PORT_ENDORSER_3}\" -l 60 {store}"
 
-    coordinator = ssh_cmd(SSH_IP_COORDINATOR, coordinator)
-
-    print(coordinator)
-    os.system(coordinator)
+    cmd = ssh_cmd(SSH_IP_COORDINATOR, coordinator)
+    print(cmd)
+    os.system(cmd)
     time.sleep(5)
 
-def setup_coordinator_sgx(store):
-    coordinator = CMD + "/coordinator -t " + LISTEN_IP_COORDINATOR + " -p " + PORT_COORDINATOR + " -r " + PORT_COORDINATOR_CTRL
-    coordinator += " -e \"http://" + LISTEN_IP_SGX_ENDORSER_1 + ":" + PORT_SGX_ENDORSER_1
-    coordinator += ",http://" + LISTEN_IP_SGX_ENDORSER_2 + ":" + PORT_SGX_ENDORSER_2
-    coordinator += ",http://" + LISTEN_IP_SGX_ENDORSER_3 + ":" + PORT_SGX_ENDORSER_3
-    coordinator += "\" -l 60"
-    coordinator += store
-
-    coordinator = ssh_cmd(SSH_IP_COORDINATOR, coordinator)
-
-    print(coordinator)
-    os.system(coordinator)
-    time.sleep(5)
-
-
-
+# Setting up endpoints
 def setup_endpoints():
-    endpoint1 = CMD + "/endpoint_rest -t " + LISTEN_IP_ENDPOINT_1 + " -p " + PORT_ENDPOINT_1
-    endpoint1 += " -c \"http://" + LISTEN_IP_COORDINATOR + ":" + PORT_COORDINATOR + "\" -l 60"
-    endpoint1 = ssh_cmd(SSH_IP_ENDPOINT_1, endpoint1)
-
-    print(endpoint1)
-    os.system(endpoint1)
+    endpoint1 = f"{CMD}/endpoint_rest -t {LISTEN_IP_ENDPOINT_1} -p {PORT_ENDPOINT_1} "
+    endpoint1 += f"-c \"http://{LISTEN_IP_COORDINATOR}:{PORT_COORDINATOR}\" -l 60"
+    cmd = ssh_cmd(SSH_IP_ENDPOINT_1, endpoint1)
+    print(cmd)
+    os.system(cmd)
 
     if HAS_LB:
-        endpoint2 = CMD + "/endpoint_rest -t " + LISTEN_IP_ENDPOINT_2 + " -p " + PORT_ENDPOINT_2
-        endpoint2 += " -c \"http://" + LISTEN_IP_COORDINATOR + ":" + PORT_COORDINATOR + "\" -l 60"
-        endpoint2 = ssh_cmd(SSH_IP_ENDPOINT_2, endpoint2)
-
-        print(endpoint2)
-        os.system(endpoint2)
+        endpoint2 = f"{CMD}/endpoint_rest -t {LISTEN_IP_ENDPOINT_2} -p {PORT_ENDPOINT_2} "
+        endpoint2 += f"-c \"http://{LISTEN_IP_COORDINATOR}:{PORT_COORDINATOR}\" -l 60"
+        cmd = ssh_cmd(SSH_IP_ENDPOINT_2, endpoint2)
+        print(cmd)
+        os.system(cmd)
 
     time.sleep(5)
 
+# Setting up the system
+def setup(store, sgx=False):
+    if not sgx:
+        setup_main_endorsers()
+        setup_coordinator(store)
+    else:
+        raise NotImplementedError("SGX setup not adapted for Azurite.")
+    setup_endpoints()
+
+# Teardown function
+def teardown(sgx=False):
+    kill_endpoints()
+    kill_coordinator()
+    kill_endorsers()
+
+# Killing endorsers
 def kill_endorsers():
-    endorser1 = ssh_cmd(SSH_IP_ENDORSER_1, "pkill endorser")
-    endorser2 = ssh_cmd(SSH_IP_ENDORSER_2, "pkill endorser")
-    endorser3 = ssh_cmd(SSH_IP_ENDORSER_3, "pkill endorser")
+    endorsers = [SSH_IP_ENDORSER_1, SSH_IP_ENDORSER_2, SSH_IP_ENDORSER_3]
+    for ip in endorsers:
+        cmd = ssh_cmd(ip, "pkill endorser")
+        print(cmd)
+        os.system(cmd)
 
-    print(endorser1)
-    os.system(endorser1)
-    print(endorser2)
-    os.system(endorser2)
-    print(endorser3)
-    os.system(endorser3)
-
-def kill_sgx_endorsers():
-    endorser1 = ssh_cmd(SSH_IP_SGX_ENDORSER_1, "pkill endorser_host")
-    endorser2 = ssh_cmd(SSH_IP_SGX_ENDORSER_2, "pkill endorser_host")
-    endorser3 = ssh_cmd(SSH_IP_SGX_ENDORSER_3, "pkill endorser_host")
-
-    print(endorser1)
-    os.system(endorser1)
-    print(endorser2)
-    os.system(endorser2)
-    print(endorser3)
-    os.system(endorser3)
-
-def kill_backup_endorsers():
-    endorser4 = ssh_cmd(SSH_IP_ENDORSER_4, "pkill endorser")
-    endorser5 = ssh_cmd(SSH_IP_ENDORSER_5, "pkill endorser")
-    endorser6 = ssh_cmd(SSH_IP_ENDORSER_6, "pkill endorser")
-
-    print(endorser4)
-    os.system(endorser4)
-    print(endorser5)
-    os.system(endorser5)
-    print(endorser6)
-    os.system(endorser6)
-
-def kill_coordinator():
-    coordinator = ssh_cmd(SSH_IP_COORDINATOR, "pkill coordinator")
-
-    print(coordinator)
-    os.system(coordinator)
-
-
+# Killing endpoints
 def kill_endpoints():
     endpoint1 = ssh_cmd(SSH_IP_ENDPOINT_1, "pkill endpoint_rest")
     print(endpoint1)
@@ -159,45 +120,11 @@ def kill_endpoints():
 
     if HAS_LB:
         endpoint2 = ssh_cmd(SSH_IP_ENDPOINT_2, "pkill endpoint_rest")
-
         print(endpoint2)
         os.system(endpoint2)
 
-def setup(store, sgx):
-    if sgx:
-        setup_sgx_endorsers()
-        setup_coordinator_sgx(store)
-    else:
-        setup_main_endorsers()
-        setup_coordinator(store)
-
-    setup_endpoints()
-
-def teardown(sgx):
-    kill_endpoints()
-    kill_coordinator()
-    if sgx:
-        kill_sgx_endorsers()
-    else:
-        kill_endorsers()
-
-def ssh_cmd(ip, cmd):
-    if LOCAL_RUN:
-        return cmd.replace('\'', '')
-    else:
-        return "ssh -o StrictHostKeyChecking=no -i " + SSH_KEY_PATH + " " + SSH_USER + "@" + ip + " " + cmd
-
-def setup_output_folder(ip, out_folder):
-    # Create output folder in case it doesn't exist
-    folder_cmd = ssh_cmd(ip, "\'mkdir -p " + out_folder + "\'")
-
-    print(folder_cmd)
-    os.system(folder_cmd)
-
-def collect_results(ip):
-    if LOCAL_RUN:
-        return ""
-    else:
-        cmd = "scp -r -i " + SSH_KEY_PATH + " " + SSH_USER + "@" + ip + ":" + OUTPUT_FOLDER + " ./"
-        print(cmd)
-        os.system(cmd)
+# Killing coordinator
+def kill_coordinator():
+    cmd = ssh_cmd(SSH_IP_COORDINATOR, "pkill coordinator")
+    print(cmd)
+    os.system(cmd)
