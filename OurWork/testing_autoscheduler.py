30fd8761ee53d61c9f75fcc9d4cbf5ae3ceb5313
Kilian Matheis <33496415+MatheiANo@users.noreply.github.com>
2025-01-06 15:37:51 +0100

    tried putting all the testing into one python script

--- OurWork/testing_autoscheduler.py
+++ OurWork/testing_autoscheduler.py
@@ -1,18 +1,29 @@
 import subprocess
+import time
+import os
+import signal
 
-# Define the commands to run in parallel
-commands = [
-    "/home/kilian/Nimble/target/release/endorser -p 9090",
-    "/home/kilian/Nimble/target/release/endorser -p 9091",
-    "/home/kilian/Nimble/target/release/coordinator -e \"http://localhost:9090,http://localhost:9091\""
-]
+# Start two terminal processes in the background
+endorser1 = subprocess.Popen(['/home/kilian/Nimble/target/release/endorser -p 9090'], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+endorser2 = subprocess.Popen(['/home/kilian/Nimble/target/release/endorser -p 9091'], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
 
-# Start the processes
-processes = [subprocess.Popen(command, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE) for command in commands]
+# Give some time for the processes to start
+time.sleep(2)
 
-# Print the output of each process
-for process in processes:
-    stdout, stderr = process.communicate()
-    print(stdout.decode())
-    if stderr:
-        print(stderr.decode())
+print('/home/kilian/Nimble/target/release/coordinator -e "http://localhost:9090,http://localhost:9091"')
+# Start another process in the background and forward its output
+coordinator = subprocess.Popen(['/home/kilian/Nimble/target/release/coordinator -e "http://localhost:9090,http://localhost:9091"'], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+
+
+# Give some time for the process to run
+time.sleep(30)
+
+# Kill one of the first two processes
+os.kill(endorser1.pid, signal.SIGTERM)
+
+# Give some time for the process to run
+time.sleep(30)
+
+# Forward the output of coordinator
+for line in coordinator.stdout:
+    print(line.decode(), end='')
