a4d1b8b7825fa421c46cdd414e01cb50b2f7ec44
Kilian Matheis <33496415+MatheiANo@users.noreply.github.com>
2025-01-06 15:43:34 +0100

    final testing script

--- OurWork/testing_autoscheduler.py
+++ OurWork/testing_autoscheduler.py
@@ -7,21 +7,24 @@ import signal
 endorser1_args = ['/home/kilian/Nimble/target/release/endorser', '-p', '9090']
 endorser2_args = ['/home/kilian/Nimble/target/release/endorser', '-p', '9091']
 coordinator_args = ['/home/kilian/Nimble/target/release/coordinator', '-e', 'http://localhost:9090,http://localhost:9091']
+
+print("Starting first endorser")
 endorser1 = subprocess.Popen(endorser1_args, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+print("Starting second endorser")
 endorser2 = subprocess.Popen(endorser2_args, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
 
 # Give some time for the processes to start
 time.sleep(2)
 
-print('/home/kilian/Nimble/target/release/coordinator -e "http://localhost:9090,http://localhost:9091"')
 # Start another process in the background and forward its output
+print("Starting coordinator")
 coordinator = subprocess.Popen(coordinator_args, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-
 
 # Give some time for the process to run
 time.sleep(30)
 
 # Kill one of the first two processes
+print("Killing first endorser")
 os.kill(endorser1.pid, signal.SIGTERM)
 
 # Give some time for the process to run
