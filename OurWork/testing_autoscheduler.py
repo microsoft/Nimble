94836ac631498ea3398b54cf6a940049858ccb77
Kilian Matheis <33496415+MatheiANo@users.noreply.github.com>
2025-01-06 15:41:13 +0100

    added args

--- OurWork/testing_autoscheduler.py
+++ OurWork/testing_autoscheduler.py
@@ -3,16 +3,19 @@ import time
 import os
 import signal
 
-# Start two terminal processes in the background
-endorser1 = subprocess.Popen(['/home/kilian/Nimble/target/release/endorser -p 9090'], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-endorser2 = subprocess.Popen(['/home/kilian/Nimble/target/release/endorser -p 9091'], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+# Start two terminal processes in the background with arguments
+endorser1_args = ['/home/kilian/Nimble/target/release/endorser', '-p', '9090']
+endorser2_args = ['/home/kilian/Nimble/target/release/endorser', '-p', '9091']
+coordinator_args = ['/home/kilian/Nimble/target/release/coordinator', '-e', 'http://localhost:9090,http://localhost:9091']
+endorser1 = subprocess.Popen(endorser1_args, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+endorser2 = subprocess.Popen(endorser2_args, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
 
 # Give some time for the processes to start
 time.sleep(2)
 
 print('/home/kilian/Nimble/target/release/coordinator -e "http://localhost:9090,http://localhost:9091"')
 # Start another process in the background and forward its output
-coordinator = subprocess.Popen(['/home/kilian/Nimble/target/release/coordinator -e "http://localhost:9090,http://localhost:9091"'], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+coordinator = subprocess.Popen(coordinator_args, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
 
 
 # Give some time for the process to run
