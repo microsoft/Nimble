cff1e941349114a7af8919f4a86f2b9ced4d7389
Jan <hampejan@protonmail.com>
2025-03-16 20:47:43 +0100

    Removed personalized changes to config

--- experiments/config.py
+++ experiments/config.py
@@ -1,4 +1,4 @@
-LOCAL_RUN = True  # set to True if you want to run all nodes and experiments locally. Else set to False.
+LOCAL_RUN = False  # set to True if you want to run all nodes and experiments locally. Else set to False.
                   # If set to True, you can ignore all the IP addresses and SSH stuff below. They won't be used.
                   # You cannot run any of the Azure table experiments locally.
 
@@ -53,6 +53,7 @@ PORT_LOAD_BALANCER = "8082"  # if no load balancer is available just use one end
 
 SSH_IP_CLIENT = "127.0.0.1"  # IP of the machine that will be running our workload generator.
 
+# If you are going to be running the reconfiguration experiment, set the backup endorsers
 # Backup Endorsers for reconfiguration experiment
 SSH_IP_ENDORSER_4 = "127.0.0.1"
 LISTEN_IP_ENDORSER_4 = "127.0.0.1"
@@ -66,6 +67,7 @@ SSH_IP_ENDORSER_6 = "127.0.0.1"
 LISTEN_IP_ENDORSER_6 = "127.0.0.1"
 PORT_ENDORSER_6 = "9096"
 
+# If you are going to be running the SGX experiment on SGX machines, set the SGX endorsers
 # SGX experiment on SGX machines
 SSH_IP_SGX_ENDORSER_1 = "127.0.0.1"
 LISTEN_IP_SGX_ENDORSER_1 = "127.0.0.1"
@@ -79,14 +81,21 @@ SSH_IP_SGX_ENDORSER_3 = "127.0.0.1"
 LISTEN_IP_SGX_ENDORSER_3 = "127.0.0.1"
 PORT_SGX_ENDORSER_3 = "9093"
 
+# Set the PATHs below to the folder containing the nimble executables (e.g. "/home/user/nimble/target/release")
+# wrk2 executable, and the directory where the logs and results should be stored.
+# We assume all of the machines have the same path.
 
-# Paths to Nimble executables and wrk2 for workload generation
-NIMBLE_PATH = ""
-NIMBLE_PATH = ""
+NIMBLE_PATH = "/home/user/nimble"
 NIMBLE_BIN_PATH = NIMBLE_PATH + "/target/release"
-WRK2_PATH = "/nix/store/bmnf0j48mppj7i24pk12qaj8jja41imx-wrk2-4.0.0-e0109df/bin"
+WRK2_PATH = NIMBLE_PATH + "/experiments/wrk"
 OUTPUT_FOLDER = NIMBLE_PATH + "/experiments/results"
 
+# Set the SSH user for the machines that we will be connecting to.
+SSH_USER = "user"                       # this is the username in the machine we'll connect to (e.g., user@IP)
+SSH_KEY_PATH = "/home/user/.ssh/id_rsa" # this is the path to private key in the current machine where you'll run this script
+
+# To use Azure storage, you need to set the STORAGE_ACCOUNT_NAME and STORAGE_MASTER_KEY environment variables
+# with the corresponding values that you get from Azure.
 # Azurite doesn't need actual Azure credentials, so you can use the following default:
 STORAGE_ACCOUNT_NAME = AZURITE_STORAGE_ACCOUNT_NAME  # Use Azurite storage account name
 STORAGE_MASTER_KEY = AZURITE_STORAGE_MASTER_KEY  # Use Azurite storage master key
